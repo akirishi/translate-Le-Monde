3aef80c1cdce78d56993ea1a4e28e4b2f787b647
akirishi <90563712+akirishi@users.noreply.github.com>
2022-03-25 16:15:06 -0300

    Add files via upload

---
+++ js/reserva.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("section.mesa").click(function () {
+        $(this).animate({ width: '100%' })
+    });
+    if ($(this).css(width = '100%')) {
+        $("section.mesa").click(function () {
+            $(this).animate({ width: '24%' })
+        }
+    });
+});
+
+$(document).ready(function() {
+    if($(".mesa").css(width < '100%')) {
+        $(".mesa").click(function(){
+            $(this).animate({width: '100%'})
+        })
+    } if else()
+})
